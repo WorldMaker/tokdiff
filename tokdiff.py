66c62afaa470f04558b0eec0aae4929cddce03d0
Max Battcher <me@worldmaker.net>
2009-08-17 01:39:18 +0000

    Tokens are a tuple and need to be string formatted as such

--- tokdiff.py
+++ tokdiff.py
@@ -24,20 +24,20 @@ sm = SequenceMatcher(None, a, b)
 for op, a1, a2, b1, b2 in sm.get_opcodes():
     if op == 'equal':
         for item in a[a1:a2]:
-            print "  %s" % item
+            print "  %s: %s" % item
     elif op == 'replace':
         print "~~~"
         for item in a[a1:a2]:
-            print "- %s" % item
+            print "- %s: %s" % item
         for item in b[b1:b2]:
-            print "+ %s" % item
+            print "+ %s: %s" % item
         print "~~~"
     elif op == 'insert':
         for item in b[b1:b2]:
-            print "+ %s" % item
+            print "+ %s: %s" % item
     elif op == 'delete':
         for item in a[a1:a2]:
-            print "- %s" % item
+            print "- %s: %s" % item
     else:
         print "<<%s>>" % op
 
